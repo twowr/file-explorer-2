3b27c44924e865a281b4bce513bd4388fc143aea
2wr <taolaolgc@gmail.com>
2023-07-16 17:29:29 +0700

    renaming file

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 import { sep } from "@tauri-apps/api/path"
 import sidebar from "./components/Sidebar.vue"
-import dir_view from "./components/Dir_View.vue"
+import file_view from "./components/Dir_View.vue"
 import { ref } from "vue"
 
 const path = ref("D:\\project\\software-projects\\file_explorer_2")
@@ -32,7 +32,7 @@ function open(dir, is_directory) {
         <sidebar class="sidebar"/>
         <div class="view_panel">
             <Suspense>
-                <dir_view class="dir_view" @open="open" @back="on_back" @submit="open" :dir="path"/>
+                <file_view class="file_view" @open="open" @back="on_back" @submit="open" :dir="path"/>
             </Suspense>
         </div>
     </div>
