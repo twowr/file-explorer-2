c04826c5ad367480c2f42d425b1e9ec7ffb67518
2wr <taolaolgc@gmail.com>
2023-07-16 20:27:50 +0700

    fix FileView.vue

--- src/components/FileView.vue
+++ src/components/FileView.vue
@@ -5,9 +5,11 @@ const emit = defineEmits(["open"])
 </script>
 
 <template>
-    <div class="entry" @click="emit('open', entry.path, entry.children != null)" v-if="entries != null" v-for="entry in props.entries">
-        <img v-if="entry.children != null" src="../assets/folder_icon.png" alt="folder">
-        <img v-else src="../assets/file_icon.png" alt="file">
-        <span class="entryName">{{ entry.name }}</span>
+    <div>
+        <div class="entry" @click="emit('open', entry.path, entry.children != null)" v-if="entries != null" v-for="entry in props.entries">
+            <img v-if="entry.children != null" src="../assets/folder_icon.png" alt="folder">
+            <img v-else src="../assets/file_icon.png" alt="file">
+            <span class="entryName">{{ entry.name }}</span>
+        </div>
     </div>
 </template>
